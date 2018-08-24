a52a99ea23a3ea02bd06561e75f436ce9ab5a15e
felipeiasi <felipeiasi@gmail.com>
2018-08-24 16:18:28 -0400

    Comentarios

--- blog_livro/mysite/blog/models.py
+++ blog_livro/mysite/blog/models.py
@@ -13,9 +13,8 @@ class PublishedManager(models.Manager):
 class Post(models.Model):
   STATUS_CHOICES = (
     ('draft', 'Rascunho'),
-    ('published', 'Publicado'),
-    
-  )
+    ('published', 'Publicado'),)
+	
   title = models.CharField(max_length=250)
   slug = models.SlugField(max_length=250, 
                          unique_for_date='publish')
@@ -44,3 +43,19 @@ class Post(models.Model):
   def __str__(self):
     return self.title
   
+class Comment (models.Model):
+	post = models.ForeignKey(Post, 
+													 on_delete=models.CASCADE,
+													related_name='comments')
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+	
+	class Meta:
+		ordering = ('created',)
+		
+	def __str__(self):
+		return 'Comment by {} on {}'.format(self.name, self.post)

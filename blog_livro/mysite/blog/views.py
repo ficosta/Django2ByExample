13f230998fe8f3b5c2e8a7b1b52a2ec026a21568
felipeiasi <felipeiasi@gmail.com>
2018-08-24 15:57:45 -0400

    Email

--- blog_livro/mysite/blog/views.py
+++ blog_livro/mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 
 # Create your views here.
 from .models import Post
@@ -22,16 +23,25 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
-	# Retrieve post by id
-	post = get_object_or_404(Post, id=post_id, status='published')
-	if request.method == 'POST':
-		# Form was submitted
-		form = EmailPostForm(request.POST)
-	if form.is_valid():
-		# Form fields passed validation
-		cd = form.cleaned_data
-		# ... send email
-	else:
-		form = EmailPostForm()
-	return render(request, 'blog/post/share.html', {'post': post,
-	'form': form})
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False 
+ 
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                                          post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com',
+ [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})

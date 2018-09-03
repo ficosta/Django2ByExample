adbe80312c1877c9f810236ebe5190c00ba89f2d
felipe <felipeiasi@gmail.com>
2018-09-03 20:00:34 -0300

    Form

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.con.auth.models import User
 
 class LoginForm(forms.Form):
 	"""Formulario de login."""
@@ -10,4 +11,13 @@ class UserRegistrationForm(forms.ModelForm):
 	"""docstring for UserRegistrationForm"""
 	password = forms.CharField(label='Password', widget=forms.PasswordInput)
 	password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
-	
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'email')
+
+	def clean_password2(self):
+		cd = self.cleaned_data
+		if cd['password'] != cd ['password2']:
+			raise forms.ValidationError('Passwords don\'t match')
+		return cd['password2']

03c73e1f1bd5c93be92628b10a6f460c745b234e
felipe <felipeiasi@gmail.com>
2018-09-03 20:00:11 -0300

    Form Login

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -5,3 +5,9 @@ class LoginForm(forms.Form):
 
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+	"""docstring for UserRegistrationForm"""
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+	
